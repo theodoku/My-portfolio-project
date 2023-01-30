47be045daf02dd063db7787c05e094b29d34a114
theophilus doku <theodoku89@gmail.com>
2023-01-30 20:43:32 +0000

    fixed linter errors

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const nav = document.getElementById('nav');
-const body = document.getElementById('body');
 
 function openMobileMenu() {
-    nav.style.display = 'block';
-  }
-  
-  function closeMobileMenu() {
-    nav.style.display = 'none';
-  }
-  
-  openMobileMenu();
-  closeMobileMenu();
-  
+  nav.style.display = 'block';
+}
+
+function closeMobileMenu() {
+  nav.style.display = 'none';
+}
+
+openMobileMenu();
+closeMobileMenu();
