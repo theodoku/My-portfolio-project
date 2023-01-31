f70e01f2a624f6fcabdee833988c2fbe0f2ae131
theophilus doku <theodoku89@gmail.com>
2023-01-31 18:16:37 +0000

    modified index.html, created works section dynamically at script.js and modified style.css

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nav = document.getElementById('nav');
 const body = document.getElementById('body');
+const pcard = document.getElementById('portfolio');
 const modal = document.getElementById('modal');
 const title = document.getElementById('text_modal');
 const image = document.getElementById('modal-im');
@@ -10,7 +11,7 @@ const source = document.getElementById('source');
 
 const cards = [
   {
-    title: 'Tonic',
+    pTitle: 'Tonic',
     image: 'images/cardImg1.png',
     longDes:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
@@ -22,7 +23,7 @@ const cards = [
     roles: ['Canopy', 'Back End Dev', 2015],
   },
   {
-    title: 'Multi-Post Stories',
+    pTitle: 'Multi-Post Stories',
     image: 'images/cardImg2.png',
     longDes:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
@@ -34,7 +35,7 @@ const cards = [
     roles: ['Canopy', 'Back End Dev', 2015],
   },
   {
-    title: 'Tonic',
+    pTitle: 'Tonic',
     image: 'images/cardImg3.png',
     longDes:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
@@ -46,7 +47,7 @@ const cards = [
     roles: ['Canopy', 'Back End Dev', 2015],
   },
   {
-    title: 'Multi-Post Stories',
+    pTitle: 'Multi-Post Stories',
     image: 'images/cardImg4.png',
     longDes:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.',
@@ -59,8 +60,38 @@ const cards = [
   },
 ];
 
+// eslint-disable-next-line no-return-assign
+cards.map((card, index) => pcard.innerHTML += `<div class="cards  rev-${index}">
+        <div class="project-image">
+          <img src="${card.image}" class="portfolio-pic   alt="Tonic" />
+        </div>
+        <div class="project-content">
+            <div class="project_name">
+              <h2 id="p-title">${card.pTitle}</h2>
+            </div>
+            <div class="p-info">
+              ${card.roles.map((role, idx) => `<div class="company">
+                <p>${role}</p>
+              </div><img class="dot-${idx}" src="images/dot.png" alt="dot" />`).join('')}
+              
+            </div>
+            <div class="description">
+              <p>
+              ${card.shortDes}
+              </p>
+            </div>
+            <ul class="languagesj">
+            ${card.languages.map((lang) => `<li class="langu">${lang}</li>`).join('')}
+            </ul>
+            <div class="button-container">
+              <a href="javascript:openModal(${index})" class="btn">See Project</a>
+            </div>
+        </div>
+      </div>
+      `);
+
 const openModal = (index) => {
-  title.innerHTML = cards[index].title;
+  title.innerHTML = cards[index].pTitle;
   image.src = cards[index].image;
   longDes.innerHTML = cards[index].longDes;
   live.href = cards[index].live;
